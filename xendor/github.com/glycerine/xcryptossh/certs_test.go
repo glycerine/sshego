f3d018b94eb761e9e966d26e636f12989fc46707
Jason E. Aten <j.e.aten@gmail.com>
2017-08-26 15:46:06 -0400

    less kexloop leaks

--- xendor/github.com/glycerine/xcryptossh/certs_test.go
+++ xendor/github.com/glycerine/xcryptossh/certs_test.go
@@ -174,6 +174,8 @@ func TestHostKeyCert(t *testing.T) {
 		},
 	}
 
+	halt := NewHalter()
+	defer halt.ReqStop.Close()
 	certSigner, err := NewCertSigner(cert, testSigners["rsa"])
 	if err != nil {
 		t.Errorf("NewCertSigner: %v", err)
@@ -199,6 +201,9 @@ func TestHostKeyCert(t *testing.T) {
 		go func() {
 			conf := ServerConfig{
 				NoClientAuth: true,
+				Config: Config{
+					Halt: halt,
+				},
 			}
 			conf.AddHostKey(certSigner)
 			_, _, _, err := NewServerConn(ctx, c1, &conf)
