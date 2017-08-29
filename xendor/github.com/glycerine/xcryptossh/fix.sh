0ebb656b015baa9dc000c16b0f5654fdd39f6bd6
Jason E. Aten <j.e.aten@gmail.com>
2017-08-29 15:53:22 -0500

    ship subdir

--- xendor/github.com/glycerine/xcryptossh/fix.sh
+++ xendor/github.com/glycerine/xcryptossh/fix.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 perl -pi -e 's$github.com/glycerine/sshego/xendor/$$g' *.go
-
+for i in agent knownhosts terminal test testdata; do
+  perl -pi -e 's$github.com/glycerine/sshego/xendor/$$g' $i/*.go
+done
