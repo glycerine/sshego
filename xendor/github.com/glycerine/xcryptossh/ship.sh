a264f5ef2147a93cb3f59749610c3232d661eefd
Jason E. Aten <j.e.aten@gmail.com>
2017-08-29 15:52:54 -0500

    ship subdir

--- xendor/github.com/glycerine/xcryptossh/ship.sh
+++ xendor/github.com/glycerine/xcryptossh/ship.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-where=~/go/src/github.com/glycerine/xcryptossh
+where=${GOPATH}/src/github.com/glycerine/xcryptossh
 cp *.go ${where}
+for i in agent knownhosts terminal test testdata; do
+  cp $i/* ${where}/$i/
+done
 cp README.md ${where}
 cd ${where}; ./fix.sh
