386da6cb58964d589d6de1e190427b3f36ef3d07
Jason E. Aten <j.e.aten@gmail.com>
2017-09-08 23:33:56 -0500

    atg. quiet down the server receiving keepalives too.

--- server.go
+++ server.go
@@ -494,7 +494,7 @@ func (a *PerAttempt) discardRequestsExceptKeepalives(ctx context.Context, in <-c
 				}
 
 				now := time.Now()
-				log.Printf("sshego server.go: discardRequestsExceptKeepalives sees keepalive %v! ping.Sent: '%v'. setting replied to now='%v'", ping.Serial, ping.Sent, now)
+				//p("sshego server.go: discardRequestsExceptKeepalives sees keepalive %v! ping.Sent: '%v'. setting replied to now='%v'", ping.Serial, ping.Sent, now)
 
 				ping.Replied = now
 				pingReplyBy, err := ping.MarshalMsg(nil)
