52e181473ccb95c24dbaabb33c8fd3f5faa795b1
J <j.e.aten@gmail.com>
2019-03-13 23:40:13 -0400

    ft. 201, 050, 401. build against 2019 mailgun instead of 2017 era mailgun. Fixes #3

--- mailgun_unix.go
+++ mailgun_unix.go
@@ -4,6 +4,7 @@
 package sshego
 
 import (
+	"context"
 	"github.com/mailgun/mailgun-go"
 	//"github.com/shurcooL/go-goon"
 )
@@ -15,13 +16,13 @@ func (c *MailgunConfig) SendEmail(senderEmail, subject, plain, html, recipEmail 
 		m.SetHtml(body)
 	*/
 
-	mg := mailgun.NewMailgun(c.Domain, c.SecretApiKey, c.PublicApiKey)
+	mg := mailgun.NewMailgun(c.Domain, c.SecretApiKey)
 	from := senderEmail
 
 	m := mg.NewMessage(from, subject, plain)
 	m.SetHtml(html)
 	m.AddRecipient(recipEmail)
 
-	_, id, err := mg.Send(m)
+	_, id, err := mg.Send(context.Background(), m)
 	return id, err
 }
